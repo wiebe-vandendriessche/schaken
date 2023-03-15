5e9d2a387e20e8d04100443986bf4c7740b3d97b
brsteven <brecht.stevens@ugent.be>
2023-03-15 13:44:57 +0100

    conflict fix

--- site/scripts/backhand.js
+++ site/scripts/backhand.js
@@ -1,16 +1,16 @@
 let len=600
 let square_size = len / 8
 let canvas = document.getElementById("canvas");
-let ofsetPiece
+let ctx = canvas.getContext("2d");
+let ofsetPiece=5;
 
-//window.onload=draw_board();
+window.onload=draw_board();
 window.onload=draw_pieces();
 
 
 function draw_board() {
     canvas.setAttribute("height", len.toString());
     canvas.setAttribute("width", len.toString());
-    let ctx = canvas.getContext("2d");
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             ctx.beginPath();
@@ -25,14 +25,14 @@ function draw_board() {
     }
 }
  function draw_pieces(){
-     let ctx = canvas.getContext("2d");
      for (let i = 0; i < 8; i++) {
          for (let j = 0; j < 8; j++) {
              let img=new Image();
-             img.onload=function (){
+             img.src="picture/b_bishop_png_128px.png";
+             img.onload= () => {
                  ctx.drawImage(img,square_size*i+ofsetPiece,square_size*j+ofsetPiece,square_size-2*ofsetPiece,square_size-2*ofsetPiece)
-             }
-             img.src="picture/b_bishop_png_128px.png"
+             };
+
          }
      }
  }
